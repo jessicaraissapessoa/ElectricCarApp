1a97a6e3d45023e0832d1b9b9c23a68faead438f
jessicaraissapessoa <jessicaraissapessoa@gmail.com>
2023-09-28 22:59:22 -0300

    Manipulando Entradas do Usuário em Apps Android / Recuperando Valor EditText com Kotlin

--- app/src/main/java/br/com/jessicaraissapessoa/eletriccarapp/MainActivity.kt
+++ app/src/main/java/br/com/jessicaraissapessoa/eletriccarapp/MainActivity.kt
@@ -2,10 +2,36 @@ package br.com.jessicaraissapessoa.eletriccarapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var preco : EditText
+    lateinit var btnCalcular : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        preco = findViewById(R.id.et_preco_kwh)
+        btnCalcular = findViewById(R.id.btn_calcular)
+
+        setupViews()
+        setupListeners()
+
+    }
+
+    fun setupViews() {
+        preco = findViewById(R.id.et_preco_kwh)
+        btnCalcular = findViewById(R.id.btn_calcular)
+    }
+
+    fun setupListeners() {
+        btnCalcular.setOnClickListener {
+            val textoDitado = preco.text
+            Log.d("texto digitado ->", textoDitado.toString())
+        }
     }
 }
