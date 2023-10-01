bccdbb18c066543d2bc1c847fc2a691cdc504e41
jessicaraissapessoa <jessicaraissapessoa@gmail.com>
2023-09-30 22:57:55 -0300

    Manipulando Entradas do Usuário em Apps Android / Event Listener em imagens

--- app/src/main/java/br/com/jessicaraissapessoa/eletriccarapp/presentation/CalcularAutonomiaActivity.kt
+++ app/src/main/java/br/com/jessicaraissapessoa/eletriccarapp/presentation/CalcularAutonomiaActivity.kt
@@ -1,14 +1,59 @@
 package br.com.jessicaraissapessoa.eletriccarapp.presentation
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import br.com.jessicaraissapessoa.eletriccarapp.R
 
 class CalcularAutonomiaActivity : AppCompatActivity() {
 
+    lateinit var preco : EditText
+    lateinit var kmpercorrido : EditText
+    lateinit var btnCalcular : Button
+    lateinit var resultado : TextView
+    lateinit var btnClose : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calcular_autonomia)
+
+        setupView()
+        setupListeners()
+    }
+
+    fun setupView() {
+        preco = findViewById(R.id.et_preco_kwh)
+        kmpercorrido = findViewById(R.id.et_km_percorrido)
+        btnCalcular = findViewById(R.id.btn_calcular)
+        resultado = findViewById(R.id.tv_resultado)
+        btnClose = findViewById(R.id.iv_close)
+    }
+
+    fun setupListeners() {
+        btnCalcular.setOnClickListener {
+            calcular()
+        }
+
+        btnClose.setOnClickListener {
+            finish()
+        }
+    }
+
+    fun calcular() {
+
+        val preco = preco.text.toString().toFloat()
+        val km = kmpercorrido.text.toString().toFloat()
+
+        val result = preco / km
+        resultado.text = result.toString()
+
+        Log.d("Preço -> ", preco.toString())
+        Log.d("Km percorrido -> ", km.toString())
+        Log.d("Resultado -> ", resultado.toString())
     }
 
 }
--- app/src/main/java/br/com/jessicaraissapessoa/eletriccarapp/presentation/MainActivity.kt
+++ app/src/main/java/br/com/jessicaraissapessoa/eletriccarapp/presentation/MainActivity.kt
@@ -2,54 +2,30 @@ package br.com.jessicaraissapessoa.eletriccarapp.presentation
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import br.com.jessicaraissapessoa.eletriccarapp.R
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var preco : EditText
-    lateinit var kmpercorrido : EditText
     lateinit var btnCalcular : Button
-    lateinit var resultado : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setupViews()
+        setupView()
         setupListeners()
     }
 
-    fun setupViews() {
-//        preco = findViewById(R.id.et_preco_kwh)
-//        kmpercorrido = findViewById(R.id.et_km_percorrido)
-//        btnCalcular = findViewById(R.id.btn_calcular)
-//        resultado = findViewById(R.id.tv_resultado)
+    fun setupView() {
         btnCalcular = findViewById(R.id.btn_calcular)
     }
 
     fun setupListeners() {
         btnCalcular.setOnClickListener {
-            //calcular()
             startActivity(Intent(this, CalcularAutonomiaActivity::class.java))
         }
-    }
-
-    fun calcular() {
-
-        val preco = preco.text.toString().toFloat()
-        val km = kmpercorrido.text.toString().toFloat()
-
-        val result = preco / km
-        resultado.text = result.toString()
-
-        Log.d("Preço -> ", preco.toString())
-        Log.d("Km percorrido -> ", km.toString())
-        Log.d("Resultado -> ", resultado.toString())
     }
 
 }
