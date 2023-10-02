8c7c0ac919caa1d7add4b134e441de9c4c844ac8
jessicaraissapessoa <jessicaraissapessoa@gmail.com>
2023-10-02 17:53:45 -0300

    Manipulando Entradas do Usuário em Apps Android / Adapters

--- app/src/main/java/br/com/jessicaraissapessoa/eletriccarapp/presentation/MainActivity.kt
+++ app/src/main/java/br/com/jessicaraissapessoa/eletriccarapp/presentation/MainActivity.kt
@@ -2,16 +2,15 @@ package br.com.jessicaraissapessoa.eletriccarapp.presentation
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.ArrayAdapter
 import android.widget.Button
-import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 import br.com.jessicaraissapessoa.eletriccarapp.R
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var btnCalcular : Button
-    lateinit var lista : ListView
+    lateinit var listaCarros : RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,15 +23,15 @@ class MainActivity : AppCompatActivity() {
 
     fun setupView() {
         btnCalcular = findViewById(R.id.btn_calcular)
-        lista = findViewById(R.id.lv_informacoes)
+        listaCarros = findViewById(R.id.rv_lista_carros)
     }
 
     fun setupList() {
         var dados = arrayOf(
             "Cupcake", "Donut", "Froyo", "Gingerbread", "Honeycomb", "Ice Cream Sandwich", "Jelly Bean"
         )
-        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, dados)
-        lista.adapter = adapter
+        //val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, dados)
+        //lista.adapter = adapter
 
     }
 
