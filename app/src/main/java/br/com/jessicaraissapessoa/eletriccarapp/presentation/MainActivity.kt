4b2b35934b78dc12e0c9b1e2c1b65b63c26d0364
jessicaraissapessoa <jessicaraissapessoa@gmail.com>
2023-10-01 23:42:54 -0300

    Manipulando Entradas do Usuário em Apps Android / ListView

--- app/src/main/java/br/com/jessicaraissapessoa/eletriccarapp/presentation/MainActivity.kt
+++ app/src/main/java/br/com/jessicaraissapessoa/eletriccarapp/presentation/MainActivity.kt
@@ -2,13 +2,16 @@ package br.com.jessicaraissapessoa.eletriccarapp.presentation
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 import br.com.jessicaraissapessoa.eletriccarapp.R
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var btnCalcular : Button
+    lateinit var lista : ListView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,10 +19,21 @@ class MainActivity : AppCompatActivity() {
 
         setupView()
         setupListeners()
+        setupList()
     }
 
     fun setupView() {
         btnCalcular = findViewById(R.id.btn_calcular)
+        lista = findViewById(R.id.lv_informacoes)
+    }
+
+    fun setupList() {
+        var dados = arrayOf(
+            "Cupcake", "Donut", "Froyo", "Gingerbread", "Honeycomb", "Ice Cream Sandwich", "Jelly Bean"
+        )
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, dados)
+        lista.adapter = adapter
+
     }
 
     fun setupListeners() {
