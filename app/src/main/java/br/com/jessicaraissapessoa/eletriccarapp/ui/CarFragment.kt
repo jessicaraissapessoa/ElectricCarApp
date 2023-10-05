a4fbc99bcaf69ba5746fc5863940822bb71627db
jessicaraissapessoa <jessicaraissapessoa@gmail.com>
2023-10-05 01:08:46 -0300

    Noções Básicas de Rede em Apps android com Kotlin / Ler JSON com o AsyncTask

--- app/src/main/java/br/com/jessicaraissapessoa/eletriccarapp/ui/CarFragment.kt
+++ app/src/main/java/br/com/jessicaraissapessoa/eletriccarapp/ui/CarFragment.kt
@@ -1,6 +1,5 @@
 package br.com.jessicaraissapessoa.eletriccarapp.ui
 
-import android.content.Intent
 import android.os.AsyncTask
 import android.os.Bundle
 import android.util.Log
@@ -13,11 +12,11 @@ import br.com.jessicaraissapessoa.eletriccarapp.R
 import br.com.jessicaraissapessoa.eletriccarapp.data.CarFactory
 import br.com.jessicaraissapessoa.eletriccarapp.ui.adapter.CarAdapter
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import org.json.JSONObject
+import org.json.JSONArray
+import org.json.JSONTokener
 import java.io.BufferedReader
 import java.io.InputStream
 import java.io.InputStreamReader
-import java.lang.Exception
 import java.net.HttpURLConnection
 import java.net.URL
 
@@ -58,11 +57,12 @@ class CarFragment : Fragment() {
 
     fun setupListeners() {
         fabCalcular.setOnClickListener {
-            startActivity(Intent(context, CalcularAutonomiaActivity::class.java))
+            MyTask().execute("https://igorbag.github.io/cars-api/cars.json")
+            //startActivity(Intent(context, CalcularAutonomiaActivity::class.java))
         }
     }
 
-    inner class GetCarInformations : AsyncTask<String, String, String>() {
+    inner class MyTask : AsyncTask<String, String, String>() {
 
         override fun onPreExecute() {
             //Executado antes de rodar
@@ -77,20 +77,13 @@ class CarFragment : Fragment() {
             try { //try/catch é sempre bom de ser usado quando dependemos de algo externo
                 val urlBase = URL(url[0])
 
-                //Para fazer uma requisição, precisamos abrir uma conexão com a internet:
-                urlConnection = urlBase.openConnection() as HttpURLConnection
+                urlConnection = urlBase.openConnection() as HttpURLConnection //abrindo conexão com a internet
+                urlConnection.connectTimeout = 60000 //para saber quanto tempo (milisegundos) que vai demorar e vamos respeitar
+                urlConnection.readTimeout = 60000 //definição do tempo de leitura com readTimeout
 
-                //Colocar um connectTimeout para saber quanto tempo (milisegundos) que vai demorar e vamos respeitar:
-                urlConnection.connectTimeout = 60000
+                var response = urlConnection.inputStream.bufferedReader().use { it.readText() } //stream dos dados que são trafegados pela internet
 
-                //Agora vamos definir o tempo de leitura com readTimeout:
-                urlConnection.readTimeout = 60000
-
-                //Agora vamos criar o imputStream que vai ser o stream dos dados que são trafegados pela internet:
-                var inString = streamToString(urlConnection.inputStream)
-
-                //Publica a inString para nosso método do background
-                publishProgress(inString)
+                publishProgress(response) //Publica progresso para nosso método do background
 
             } catch (ex: Exception) {
                 Log.e("Erro", "Erro ao ralizar processamento...")
@@ -103,9 +96,26 @@ class CarFragment : Fragment() {
 
         override fun onProgressUpdate(vararg values: String?) { //Que é quando nossos dados estarão sendo atualizados
             try {
-                var json  : JSONObject
-                values[0]?.let { //Só pega se for não nulo
-                    json  = JSONObject(it)
+                val jsonArray = JSONTokener(values[0]).nextValue() as JSONArray
+
+                for (i in 0 until jsonArray.length()) {
+                    val id = jsonArray.getJSONObject(i).getString("id")
+                    Log.d("ID ->", id)
+
+                    val preco = jsonArray.getJSONObject(i).getString("preço")
+                    Log.d("preço ->", preco)
+
+                    val bateria = jsonArray.getJSONObject(i).getString("bateria")
+                    Log.d("bateria ->", bateria)
+
+                    val potencia = jsonArray.getJSONObject(i).getString("potência")
+                    Log.d("potência ->", potencia)
+
+                    val recarga = jsonArray.getJSONObject(i).getString("recarga")
+                    Log.d("recarga ->", recarga)
+
+                    val urlPhoto = jsonArray.getJSONObject(i).getString("urlPhoto")
+                    Log.d("urlPhoto ->", urlPhoto)
                 }
 
             } catch (ex: Exception) {
@@ -113,24 +123,24 @@ class CarFragment : Fragment() {
             }
         }
 
-        fun streamToString(inputStream: InputStream) : String {
-
-            val bufferReader = BufferedReader(InputStreamReader(inputStream))
-            var line : String
-            var result = ""
-
-            try {
-                do {
-                    line = bufferReader.readLine()
-                    line?.let {
-                        result += line
-                    }
-                } while (line != null)
-            } catch (ex: Exception) {
-                Log.e("Erro", "Erro ao parcelar Stream")
-            }
-            return result
-        }
+//        fun streamToString(inputStream: InputStream) : String {
+//
+//            val bufferReader = BufferedReader(InputStreamReader(inputStream))
+//            var line : String
+//            var result = ""
+//
+//            try {
+//                do {
+//                    line = bufferReader.readLine()
+//                    line?.let {
+//                        result += line
+//                    }
+//                } while (line != null)
+//            } catch (ex: Exception) {
+//                Log.e("Erro", "Erro ao parcelar Stream")
+//            }
+//            return result
+//        }
 
     }
 }
