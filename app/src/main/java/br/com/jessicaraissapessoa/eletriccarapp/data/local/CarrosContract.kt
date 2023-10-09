6e6152d59702cfc356d2d03a25f8a7301211c17c
jessicaraissapessoa <jessicaraissapessoa@gmail.com>
2023-10-08 22:24:01 -0300

    Armazenamento de Dados Essencial em Apps Android com Kotlin / Adicionando estrutura de tabels de carros

--- app/src/main/java/br/com/jessicaraissapessoa/eletriccarapp/data/local/CarrosContract.kt
+++ app/src/main/java/br/com/jessicaraissapessoa/eletriccarapp/data/local/CarrosContract.kt
@@ -5,11 +5,23 @@ import android.provider.BaseColumns
 object CarrosContract {
     object CarEntry : BaseColumns {
         const val TABLE_NAME = "car"
-        const val COLLUMN_NAME_ID = "id"
         const val COLLUMN_NAME_PRECO = "preco"
         const val COLLUMN_NAME_BATERIA = "bateria"
         const val COLLUMN_NAME_POTENCIA = "potencia"
         const val COLLUMN_NAME_RACARGA = "recarga"
         const val COLLUMN_NAME_URL_PHOTO = "url_photo"
     }
+
+    //Estrutura do banco
+    const val TABLE_CAR =
+        "CREATE TABLE ${CarEntry.TABLE_NAME} (" +
+                "${BaseColumns._ID} INTEGER PRIMARY KEY," +
+                "${CarEntry.COLLUMN_NAME_PRECO} TEXT," +
+                "${CarEntry.COLLUMN_NAME_BATERIA} TEXT," +
+                "${CarEntry.COLLUMN_NAME_POTENCIA} TEXT," +
+                "${CarEntry.COLLUMN_NAME_RACARGA} TEXT," +
+                "${CarEntry.COLLUMN_NAME_URL_PHOTO} TEXT)"
+
+    const val SQL_DELETE_ENTRIES =
+        "DROP TABLE IF EXISTS ${CarEntry.TABLE_NAME}"
 }
